243e95fd38235d0dc22b4485b0b5962fda62bfbc
Fernando Bordin <fernandobordin07@hotmail.com>
2025-07-15 22:34:39 -0300

    Adicionando configuração para hospedagem no Python Anywhere

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://vitorbri.pythonanywhere.com/api/',
+    baseUrl: 'http://localhost:8000/api/',
     prepareHeaders: (headers, { endpoint }) => {
       const token = localStorage.getItem('access_token')
       if (token && endpoint !== 'register') {
