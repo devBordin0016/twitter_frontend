bb5c03c39861f0cbb93aed288315d73813558bba
Fernando Bordin <fernandobordin07@hotmail.com>
2025-07-16 18:59:07 -0300

    Conexão entre o backend e o frontend

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:8000/api/',
+    baseUrl: 'https://devbordin.pythonanywhere.com/api/',
     prepareHeaders: (headers, { endpoint }) => {
       const token = localStorage.getItem('access_token')
       if (token && endpoint !== 'register') {
